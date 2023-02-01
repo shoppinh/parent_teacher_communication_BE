5b1f45aa35dd5e99cd18a8307d697c247745d15b
kien.mac <kien.mac@niteco.se>
2023-02-01 17:29:54 +0700

    [Feature][Teacher-Assignment]: Done CRUD actions, need more testing on whole admin api route

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -3,11 +3,61 @@ import { BaseService } from '../shared/service/base.service';
 import { Teacher, TeacherDocument } from './schema/teacher.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { TeacherSortOrder } from '../admin/dto/get-all-teacher.dto';
+import { isEmptyObject } from '../shared/utils';
 
 @Injectable()
 export class TeacherService extends BaseService<Teacher> {
   constructor(@InjectModel(Teacher.name) private readonly _teacherModelDocument: Model<TeacherDocument>) {
     super();
     this.model = _teacherModelDocument;
+  }
+
+  async getTeacherList(sort: Partial<TeacherSortOrder>, search: string, limit: number, skip: number) {
+    const aggregation = this.model.aggregate();
+    const paginationStage = [];
+    if (search) {
+      aggregation.match({
+        $or: [
+          {
+            username: { $eq: search },
+          },
+          {
+            mobilePhone: { $eq: parseInt(search) },
+          },
+          {
+            email: { $eq: search },
+          },
+        ],
+      });
+    }
+    if (skip) {
+      paginationStage.push({
+        $skip: skip,
+      });
+    }
+    if (limit) {
+      paginationStage.push({
+        $limit: limit,
+      });
+    }
+
+    if (sort && !isEmptyObject(sort)) {
+      aggregation.sort(sort).collation({ locale: 'en' });
+    }
+    return aggregation
+      .facet({
+        totalRecords: [
+          {
+            $count: 'total',
+          },
+        ],
+        data: paginationStage,
+      })
+      .exec();
+  }
+
+  async getTeacherDetail(id: string) {
+    return this.model.findById(id).populate('userId').exec();
   }
 }
