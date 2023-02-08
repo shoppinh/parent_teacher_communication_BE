014c8f7b054a0b6fb181f9125ba13b2e5de3b1fb
kien.mac <mactrungkien2000@gmail.com>
2023-02-08 23:50:23 +0700

    [Feature][Post-Controller]: Need to add react to comment function and refactor, move all validation process in service to controller, service is only use to query db

--- src/post/schema/post.schema.ts
+++ src/post/schema/post.schema.ts
@@ -3,6 +3,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 import { User } from '../../user/schema/user.schema';
 import { Class } from '../../class/schema/class.schema';
+import { ConstantPostType } from '../../shared/utils/constant/post';
 
 export type PostDocument = Post & Document;
 
@@ -17,12 +18,16 @@ export class Post extends BaseSchema {
   @Prop({ type: Types.ObjectId, ref: Class.name, required: true })
   classId: Class;
 
-  @Prop({ required: false })
-  title?: string;
+  @Prop({ required: true })
+  title: string;
   @Prop({ required: false })
   content?: string;
   @Prop({ required: false })
-  type?: string;
+  description?: string;
+  @Prop({ required: false })
+  coverImg?: string;
+  @Prop({ required: true, default: ConstantPostType.PUBLIC })
+  type: string;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
