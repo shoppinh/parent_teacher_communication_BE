bcda01efd43ac23e889e3813deb74eef96c37fb0
kien.mac <mactrungkien2000@gmail.com>
2023-04-16 19:16:18 +0700

    feat (leave-form): Continue to update leave form logic for teacher role

--- src/parent/parent.controller.ts
+++ src/parent/parent.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, forwardRef, Get, HttpCode, HttpException, HttpStatus, Inject, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, forwardRef, Get, HttpCode, HttpException, HttpStatus, Inject, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { ParentService } from './parent.service';
 import { ApiBadRequestResponse, ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
 import { Roles } from '../shared/decorator/roles.decorator';
@@ -57,6 +57,37 @@ export class ParentController {
         leaveDate: new Date(leaveDate),
       };
       const result = await this._leaveFormService.create(leaveFormInstance);
+      return new ApiResponse(result);
+    } catch (error) {
+      throw new HttpException(error?.response ?? (await i18n.translate(`message.internal_server_error`)), error?.status ?? HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: error,
+      });
+    }
+  }
+
+  @Put('leave-form/:id')
+  @ApiBearerAuth()
+  @Roles(ConstantRoles.PARENT)
+  @ApiBadRequestResponse({ type: ApiException })
+  @HttpCode(HttpStatus.OK)
+  async updateLeaveForm(@Body() updateLeaveFormDto: Partial<AddLeaveFormDto>, @GetUser() user: User, @I18n() i18n: I18nContext, @Param('id') formId: string) {
+    try {
+      const { studentId, classId, reason, title, leaveDate } = updateLeaveFormDto;
+      await validateFields({ classId, formId, studentId }, `common.required_field`, i18n);
+      const studentExisted = await this._studentService.findById(studentId);
+      if (!studentExisted) {
+        throw new HttpException(await i18n.translate(`message.nonexistent_student`), HttpStatus.NOT_FOUND);
+      }
+      const classExisted = await this._classService.findById(classId);
+      if (!classExisted) {
+        throw new HttpException(await i18n.translate(`message.nonexistent_class`), HttpStatus.NOT_FOUND);
+      }
+      const leaveFormInstance = {
+        reason,
+        title,
+        leaveDate: new Date(leaveDate),
+      };
+      const result = await this._leaveFormService.update(formId, leaveFormInstance);
       return new ApiResponse(result);
     } catch (error) {
       throw new HttpException(error?.response ?? (await i18n.translate(`message.internal_server_error`)), error?.status ?? HttpStatus.INTERNAL_SERVER_ERROR, {
